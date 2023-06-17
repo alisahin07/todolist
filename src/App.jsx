5604b7a98002644000b0389b93c8fb7e1e37bf42
alisahin07 <alisahin172@gmail.com>
2023-06-17 23:53:48 +0300

    react Todo list basic

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,30 @@
 
 import { useEffect, useState } from "react"
 import "./style.css"
-import NewTodoForm from "./NewTodoForm";
-import { Todolist } from "./Todolist";
+ 
  
 export default function App(){
+const [newItem,setNewItem]=useState("") 
+const [todos,setTodos]=useState([])
  
-const [todos,setTodos]=useState(()=>{
-  const localValue=localStorage.getItem("ITEMS")
-  if (localValue== null) return []
-  return JSON.parse(localValue)
-});
+
+function handlesubmit(e){
+  
+  e.preventDefault(); 
+  setTodos(currentTodos=>[...currentTodos,
+    {id:crypto.randomUUID(),
+     title:newItem,
+     completed:false 
+    
+    }]
+    
+    )
+    setNewItem("");
+   console.log("girdi")
+}
 
 
-
-
-useEffect (() =>{localStorage.setItem("ITEMS",JSON.stringify(todos))},[todos] )
+ 
 function addTodo(title)
 {
     setTodos(currentTodos=>[...currentTodos,{id:crypto.randomUUID(),title,completed:false}])
@@ -37,10 +46,27 @@ function deleteTodo(id){
 
 return (
   <>
-  asdasd
-  <NewTodoForm onSubmit1={addTodo} />
-  <h1>Todo List</h1>
-  <Todolist todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    <form onSubmit={handlesubmit} className="new-item-form">
+      <div className="form-row" >
+        <label htmlFor="a">
+          <input type="text"   id="a" value={newItem} onChange={e=>setNewItem(e.target.value)} />
+        </label>
+      </div>
+       <button className="btn">ADD</button>
+    </form>
+    <h1>To Do LIST</h1>
+    <ul className="list">
+        {todos.map(
+          todo=>{
+            return(  
+          <li key={todo.id}>
+             <label > asdasd <input type="checkbox"  checked={todo.completed} onChange={e=> toggleTodo(todo.id,e.target.value)} /> </label>
+             <button onClick={()=>deleteTodo(todo.id)} className="btn btn-danger">DELETE</button>
+          </li>
+          )
+          }    )}
+    </ul>
+
   </>
 )
 }
